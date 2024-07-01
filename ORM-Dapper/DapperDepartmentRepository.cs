d572b4b8cd26241ef32c4750a735e8ab5c46b178
“Noah <urbannoah@yahoo.com>
2024-07-01 11:08:38 -0400

    exercise 1

---
+++ ORM-Dapper/DapperDepartmentRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data.Common;
+using Dapper;
+using ORM_Dapper;
+
+public class DapperDepartmentRepository : IDepartmentRepository
+{
+    private readonly IDbConnection _connection;
+
+    public DapperDepartmentRepository(IDbConnection connection)
+    {
+        _connection = connection;
+    }
+
+    public IEnumerable<Department> GetAllDepartments()
+    {
+        return _connection.Query<Department>("SELECT * FROM Departments;").ToList();
+    }
+
+    public void InsertDepartment(string newDepartmentName)
+    {
+        _connection.Execute("INSERT INTO departments (Name) VALUES (@departmentname);",
+         new { departmentName = newDepartmentName });
+    }
+}
+
---
+++ ORM-Dapper/IDepartmentRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data.Common;
+using Dapper;
+using ORM_Dapper;
+
+namespace ORM_Dapper
+{
+	public interface IDepartmentRepository
+	{
+		IEnumerable<Department> GetAllDepartments();
+	}
+}
+
